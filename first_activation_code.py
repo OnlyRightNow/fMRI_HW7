c6b61abeb51407dcffb16cb52de9a185ccd0a4aa
Sebastian <yodaxx3@gmail.com>
2019-11-04 16:23:32 +0800

    + finished exercises

--- first_activation_code.py
+++ first_activation_code.py
@@ -4,45 +4,65 @@
 #: import common modules
 import numpy as np  # the Python array package
 import matplotlib.pyplot as plt  # the Python plotting package
+import nibabel as nib
+
 # Display array values to 6 digits of precision
 np.set_printoptions(precision=4, suppress=True)
 
-#- Read the file into an array called "task".
-#- "task" should have 3 columns (onset, duration, amplitude)
-#- HINT: np.loadtxt
+# - Read the file into an array called "task".
+# - "task" should have 3 columns (onset, duration, amplitude)
+# - HINT: np.loadtxt
+file = np.loadtxt('ds114_sub009_t2r1_cond.txt')
+print('shape file: ', file.shape)
+# - Select first two columns and divide by TR
+TR = 2.5  # take same as in first exercise
+columns = file[:, :2] / TR
 
-#- Select first two columns and divide by TR
-
-#- Load the image and check the image shape to get the number of TRs
-
-#- Make new zero vector
-
+# - Load the image and check the image shape to get the number of TRs
+img_file = nib.load('ds114_sub009_t2r1.nii')
+print('img_file shape: ', img_file.shape)
+# - Make new zero vector
+zeros_img_file = np.zeros(img_file.shape[-1])
 #: try running this if you don't believe me
 len(range(4, 16))
 
-#- Fill in values of 1 for positions of on blocks in time course
+# - Fill in values of 1 for positions of on blocks in time course
+oneset_duration = np.round(columns).astype(int)
+time = zeros_img_file
+for onset, duration in oneset_duration:
+    time[onset: onset + duration] = 1
 
-#- Plot the time course
+# - Plot the time course
+plt.plot(time)
+plt.show()
+# - Make two boolean arrays encoding task, rest
+task = (time == 1)
+rest = (time == 0)
 
-#- Make two boolean arrays encoding task, rest
-
-
-#- Create a new 4D array only containing the task volumes
-
-#- Create a new 4D array only containing the rest volumes
-
-#- Create the mean volume across all the task volumes.
-#- Then create the mean volume across all the rest volumes
-
-#- Create a difference volume
-
-#- Show a slice over the third dimension
-
-#- Calculate the SD across voxels for each volume
-#- Identify the outlier volume
-
-#- Use slicing to remove outlier volume from rest volumes
-
-#- Make new mean for rest volumes, subtract from task mean
-
-#- show same slice from old and new difference volume
+# - Create a new 4D array only containing the task volumes
+img_data = img_file.get_data()
+task_vol = img_data[..., task]
+# - Create a new 4D array only containing the rest volumes
+rest_vol = img_data[..., rest]
+# - Create the mean volume across all the task volumes.
+# - Then create the mean volume across all the rest volumes
+task_mean = task_vol.mean(axis=-1)
+rest_mean = rest_vol.mean(axis=-1)
+# - Create a difference volume
+difference_vol = task_mean - rest_mean
+# - Show a slice over the third dimension
+plt.imshow(difference_vol[:, :, 22])
+plt.show()
+# - Calculate the SD across voxels for each volume
+# - Identify the outlier volume
+voxels = img_data.reshape((-1, img_data.shape[-1]))
+sd = np.std(voxels, axis=0)
+print('outlier volume: ', np.argmax(sd))
+# - Use slicing to remove outlier volume from rest volumes
+rest_vol_sliced = rest_vol[..., 1:]
+# - Make new mean for rest volumes, subtract from task mean
+rest_sliced_mean = rest_vol_sliced.mean(axis=-1)
+difference_vol_sliced = task_mean - rest_sliced_mean
+# - show same slice from old and new difference volume
+plt.imshow(difference_vol_sliced[:, :, 22])
+plt.show()
--- voxel_correlation_code.py
+++ voxel_correlation_code.py
@@ -8,25 +8,37 @@ import numpy as np  # the Python array package
 import matplotlib.pyplot as plt  # the Python plotting package
 import nibabel as nib
 
-#- import events2neural from stimuli module
+# - import events2neural from stimuli module
+from stimuli import events2neural
 
-#- Load the ds114_sub009_t2r1.nii image
-
-#- Get the number of volumes in ds114_sub009_t2r1.nii
-
-#: TR
+# - Load the ds114_sub009_t2r1.nii image
+file = nib.load('ds114_sub009_t2r1.nii')
+# - Get the number of volumes in ds114_sub009_t2r1.nii
+n_vols = file.shape[-1]
+print('number of volumes in file: ', n_vols)
+#: TR (time between scans)
 TR = 2.5
 
-#- Call the events2neural function to generate the on-off values for
-#- each volume.  Plot these values.
-
-#- Drop the first 4 volumes, and the first 4 on-off values.
-
-#- Make a brain-volume-size array of 0 to hold the correlations
-
-#- Loop over all voxel indices.
-#- Extract the voxel time courses at each voxel.
-#- Get correlation value for voxel time course with on-off vector.
-#- Fill value in the correlations array.
-
-#- Plot the middle slice of the third axis from the correlations array
+# - Call the events2neural function to generate the on-off values for
+# - each volume.  Plot these values.
+on_off = events2neural('ds114_sub009_t2r1_cond.txt', 2.5, n_vols) # work also for other values than 2.5, like 0.25
+plt.plot(on_off)
+plt.show()
+# - Drop the first 4 volumes, and the first 4 on-off values.
+image_data = file.get_data()
+image_data = image_data[..., 4:]
+on_off = on_off[4:]
+# - Make a brain-volume-size array of 0 to hold the correlations
+zeros = np.zeros(image_data.shape[:-1])
+# - Loop over all voxel indices.
+# - Extract the voxel time courses at each voxel.
+# - Get correlation value for voxel time course with on-off vector.
+# - Fill value in the correlations array.
+corr = zeros
+for i in range(image_data.shape[0]):
+    for j in range(image_data.shape[1]):
+        for k in range(image_data.shape[2]):
+            corr[i, j, k] = np.corrcoef(on_off, image_data[i, j, k])[1, 0]
+# - Plot the middle slice of the third axis from the correlations array
+plt.imshow(corr[:, :, 15])
+plt.show()
